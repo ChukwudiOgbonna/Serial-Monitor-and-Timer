7962a016eae139d77c617ef00db158ca244591f4
ChukwudiOgbonna <43212263+ChukwudiOgbonna@users.noreply.github.com>
2021-02-05 06:35:35 +0000

    Add files via upload

---
+++ serial monitor/sketch_jan31a/sketch_jan31a.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("My name is Ogbonna Chukwudi and my matric number is 16CJ020749");
+  delay(3000);
+}
